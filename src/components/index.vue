dc16c52fd57eaf92eef9f44508037e5c838b67fb
zoy <zoy@MacBook-Air-2012.local>
2020-02-22 13:09:54 +0900

    jsonファイルを画面表示できるようにする #2

---
+++ src/components/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="hello">
+    <h1>vuelog</h1>
+    <li v-for="a in articles" :key="a.id">
+      {{ a.id}}
+      {{ a.name}}
+    </li>
+  </div>
+</template>
+
+<script>
+import articles from '../assets/article.json'
+
+export default {
+  name: 'index',
+  props: {
+    msg: String
+  },
+  data () {
+    return {
+      articles: articles,
+      key: '',
+      id: ''
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
