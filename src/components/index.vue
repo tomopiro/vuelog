6211d88e01de5706832bed72510afc0c723e9f20
zoy <zoy@MacBook-Air-2012.local>
2020-02-24 14:36:23 +0900

    フォーマッターの追加（prettier） #9

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,45 +1,47 @@
 <template>
-  <div class="hello">
-    <h1>vuelog</h1>
-    <ul v-for="s in summary.fileMap" :key="s.created_at">
-      <li>タイトル：{{ s.title }}</li><br>
-      <li>投稿日：{{ s.created_at }}</li>
-    </ul>
-  </div>
+	<div class="hello">
+		<h1>vuelog</h1>
+		<ul v-for="s in summary.fileMap" :key="s.created_at">
+			<li>タイトル：{{ s.title }}</li>
+			<br />
+			<li>投稿日：{{ s.created_at }}</li>
+		</ul>
+	</div>
 </template>
 
 <script>
-import summary from '../json/summary.json'
+import summary from '../json/summary.json';
 
 export default {
-  name: 'index',
-  props: {
-    msg: String
-  },
-  data () {
-    return {
-      summary: summary,
-      key: '',
-      id: ''
-    }
-  }
-}
+	name: 'index',
+	props: {
+		msg: String,
+	},
+	data() {
+		return {
+			summary,
+			key: '',
+			id: '',
+		};
+	},
+};
+console.log('test');
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
-  margin: 40px 0 0;
+	margin: 40px 0 0;
 }
 ul {
-  list-style-type: none;
-  padding: 0;
+	list-style-type: none;
+	padding: 0;
 }
 li {
-  display: inline-block;
-  margin: 0 10px;
+	display: inline-block;
+	margin: 0 10px;
 }
 a {
-  color: #42b983;
+	color: #42b983;
 }
 </style>
