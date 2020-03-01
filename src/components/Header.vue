b977d4fe5c5d7bfb39c920713c8158299c608f6f
zoy <zoy@MacBook-Air-2012.local>
2020-03-01 09:44:48 +0900

    Headerコンポーネントの作成 #19

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <b-navbar
+      toggleable="lg"
+      type="dark"
+      variant="dark"
+    >
+      <b-navbar-brand href="#">
+        Vuelog
+      </b-navbar-brand>
+      <div>
+        <a href="https://twitter.com/tomo_moog">
+        <v-fa
+          :icon="['fab', 'twitter']"
+          size="5x"
+          inverse
+        />
+        </a>
+        <a href="https://github.com/tomopiro">
+        <v-fa
+          :icon="['fab', 'github']"
+          size="5x"
+          inverse
+        />
+        </a>
+      </div>
+    </b-navbar>
+  </div>
+</template>
