5515b9314a439f85d771eefb1852d4a230ddb0eb
zoy <zoy@MacBook-Air-2012.local>
2020-04-09 20:14:23 +0900

    ヘッダー修正（中央寄せ、アイコン縮小） #45

--- src/router.js
+++ src/router.js
@@ -18,5 +18,13 @@ export default new Router({
       name: 'Article',
       component: Article,
     },
+    {
+      path: '/twitter',
+      beforeEnter() { window.open('http://twitter.com/tomo_moog'); },
+    },
+    {
+      path: '/github',
+      beforeEnter() { window.open('http://github.com/tomopiro'); },
+    },
   ],
 });
